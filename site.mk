b8c0177f5211389d399d8dfdf37c90da15015b41
Andreas <mail@rohrmann.com>
2018-02-14 08:15:51 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,33 +1,42 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
-	gluon-announced \
+    gluon-respondd \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+    gluon-config-mode-core \
+	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-legacy \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-private-wifi \
-	gluon-next-node \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+    gluon-web-network \
+    gluon-web-wifi-config \
+    gluon-web-node-role \
+	gluon-web-private-wifi \
 	gluon-mesh-vpn-fastd \
+    #gluon-mesh-vpn-tunneldigger \
 	gluon-radvd \
 	gluon-wan-dnsmasq \
+    gluon-setup-mode \
 	gluon-status-page \
+	gluon-authorized-keys \
+    #respondd-module-airtime \
 	iwinfo \
 	iptables \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.6~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.9.0-FF-$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
+
+GLUON_REGION ?= eu
+
+GLUON_LANGS ?= de en
